e2588191a4f100308338ba4ee27646006c5e3838
Elbert Nathanael <elbertwithane@gmail.com>
2023-01-04 10:10:35 +0800

    fixed interactive link

--- src/articles/data/interactiveNarrative.jsx
+++ src/articles/data/interactiveNarrative.jsx
@@ -8,7 +8,7 @@ export default function InteractiveNarrative(){
             <div>Inspired by Netflix's interactive narrative projects like 'bandersnatch'. Genesis wanted to re-create these projects here in Singapore. This project is our showpiece and workflow discovering process. Nailing down the pipeline and developing the required foundations to replicate the interactive medium.</div>
             <img className="imageFit" src={getAssetUrl('InteractiveNarrative_Flowchart.png')} alt="" />
             <div>On top of being coordinating with the film crew producing the film, I was the developer for this project. Had to create a custom video player on the web so that all the current relevant story branches are pre-loaded so you can experience the narrative seamlessly in HD</div>
-            <a className="linkOut interactive" href="https:/genesismotiontech.com/interactive-video">Check it out here!</a>
+            <a className="linkOut interactive" href="https://genesismotiontech.com/interactive-video">Check it out here!</a>
         </div>
     )
 }
